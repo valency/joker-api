7a46d5da477edeaa28a61e6766052c6a0fed4e7d
Ye Ding <guiewy@gmail.com>
2015-06-04 19:43:52 +0800

    v0.6.4

--- joker/views.py
+++ joker/views.py
@@ -36,7 +36,7 @@ def get_cust_all(request):
             page = int(request.GET["draw"])
             size = int(request.GET["length"])
             cust = Paginator(Customer.objects.all(), size)
-            return Response(cust.page(page).object_list)
+            return Response(CustomerSerializer(cust.page(page), many=True).data)
         except ObjectDoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         except TypeError:
