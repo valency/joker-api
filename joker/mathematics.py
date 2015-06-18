8413d8dd9fbe451acbe6abdabb0e000f53418488
Ye Ding <guiewy@gmail.com>
2015-06-19 02:03:53 +0800

    v0.6.18

--- joker/mathematics.py
+++ joker/mathematics.py
@@ -1,8 +1,8 @@
 from statsmodels.tools import categorical
 from sklearn.cluster import KMeans
 
-from models import *
 from common import *
+from serializers import *
 
 
 class Mathematics:
@@ -70,11 +70,11 @@ class Mathematics:
         k_means.fit(cust_matrix)
         result = []
         for i in range(0, len(id_list)):
-            entity = {
+            result.append({
                 "id": id_list[i],
                 "cluster": k_means.labels_[i]
-            }
-            for h in header:
-                entity[h] = cust_set.get(id=id_list[i]).__dict__[h]
-            result.append(entity)
-        return result
+            })
+        return {
+            "result": result,
+            "cust": CustomerSerializer(cust_set, many=True).data
+        }
