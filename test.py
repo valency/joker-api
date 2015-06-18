6ac15b897ae5dcb2ab86969130597a77562aae36
Ye Ding <guiewy@gmail.com>
2015-06-19 00:46:08 +0800

    v0.6.18

--- test.py
+++
@@ -1,5 +0,0 @@
-import numpy
-
-cust_matrix = numpy.array([[0.5, 0.3], [0.2, 0.2]])
-weight = numpy.array("1,2".split(","))
-print numpy.nan_to_num(numpy.multiply(cust_matrix, numpy.array([numpy.array(weight)] * 2)))
