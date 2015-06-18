09b73386c89595d879972b6cea55eb2d5c5ac02f
Ye Ding <guiewy@gmail.com>
2015-06-19 00:07:23 +0800

    v0.6.18

--- joker/mathematics.py
+++ joker/mathematics.py
@@ -1,7 +1,7 @@
-import numpy
 from statsmodels.tools import categorical
 
 from models import *
+from common import *
 
 
 class Mathematics:
@@ -60,10 +60,10 @@ class Mathematics:
                 cust_matrix = cust_column
             else:
                 cust_matrix = numpy.column_stack((cust_matrix, cust_column))
-        # # Normalize
-        # cust_matrix = Common.scale_linear_by_column(cust_matrix)
-        # # Weight
-        # cust_matrix = numpy.nan_to_num(numpy.multiply(cust_matrix, numpy.array([numpy.array(weight)] * cust_set.count())))
+        # Normalize
+        cust_matrix = Common.scale_linear_by_column(cust_matrix)
+        # Weight
+        cust_matrix = numpy.nan_to_num(numpy.multiply(cust_matrix, numpy.array([numpy.array(weight)] * cust_set.count())))
         # # Clustering
         # k_means = KMeans(init="k-means++", n_clusters=n_clusters)
         # k_means.fit(cust_matrix)
