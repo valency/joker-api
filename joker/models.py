ec4b7495c462c0705fb3db506e8954142c2beae3
Ye Ding <guiewy@gmail.com>
2015-06-18 17:40:31 +0800

    v0.6.18

--- joker/models.py
+++ joker/models.py
@@ -14,6 +14,7 @@ class Prediction(models.Model):
 
 class Customer(models.Model):
     id = models.IntegerField(primary_key=True)
+    cust_code = models.IntegerField(null=True)
     age = models.IntegerField(null=True)
     gender = models.CharField(max_length=1, null=True)
     yrs_w_club = models.IntegerField(null=True)
