59cf6ec81e298d21d60e48a369f6b002fd4c81d6
Ye Ding <guiewy@gmail.com>
2015-06-29 02:32:09 +0800

    v0.6.28

---
+++ joker/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from models import *
+
+admin.site.register(Configuration)
+admin.site.register(Account)
+admin.site.register(Prediction)
+admin.site.register(Customer)
