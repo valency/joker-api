47ea9a2f69253704ca2cf981263adbcc5bae2d0e
Ye Ding <guiewy@gmail.com>
2015-06-28 22:17:31 +0800

    v0.6.28

--- joker/urls.py
+++ joker/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     url(r'^api/cust/histogram/$', views.histogram),
     url(r'^api/cust/kmeans/$', views.kmeans),
     url(r'^api/cust/dist/$', views.cust_dist),
+    url(r'^api/user/login/$', views.login),
+    url(r'^api/user/register/$', views.register),
+    url(r'^api/user/change_password/$', views.change_password),
     url(r'^api/csv_to_json/$', views.csv_to_json),
-    url(r'^api/login/$', views.login)
 ]
