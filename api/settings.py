626349ba2894a63f4bc4d3755395a9e1101f8d2e
Ye Ding <guiewy@gmail.com>
2015-07-13 16:16:38 +0800

    v0.7.13

--- api/settings.py
+++ api/settings.py
@@ -14,7 +14,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    # 'corsheaders',
     'rest_framework',
     'joker'
 )
