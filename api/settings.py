6043d2440345fc2e059779dd55d791ae70c3dac4
Ye Ding <guiewy@gmail.com>
2015-07-13 15:49:42 +0800

    v0.7.13

--- api/settings.py
+++ api/settings.py
@@ -86,3 +86,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# HTTPS
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
