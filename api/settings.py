01f3f122e957c00de3da61c0992a576d28770e47
Ye Ding <guiewy@gmail.com>
2015-07-13 15:57:42 +0800

    v0.7.13

--- api/settings.py
+++ api/settings.py
@@ -55,9 +55,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-    'nicpu3.cse.ust.hk'
-)
+CORS_ORIGIN_WHITELIST = ()
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
