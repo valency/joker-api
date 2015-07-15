644e111ef9d93dfa6b3bbeba53c8c32fd356b7b3
Ye Ding <guiewy@gmail.com>
2015-07-15 16:30:58 +0800

    v0.7.15

--- joker/admin.py
+++ joker/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from models import *
 
-admin.site.register(Configuration)
+admin.site.register(User)
 admin.site.register(Account)
-admin.site.register(Prediction)
-admin.site.register(Customer)
+admin.site.register(Configuration)
+admin.site.register(Customer1)
+admin.site.register(Customer2)
