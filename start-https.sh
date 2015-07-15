37a71e0cbc285b3700269cba3b6429e37ddca72d
Ye Ding <guiewy@gmail.com>
2015-07-15 17:22:17 +0800

    v0.7.15

--- start-https.sh
+++ start-https.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo nginx -s stop
 killall gunicorn
-gunicorn --limit-request-line 8190 -b unix:`pwd`/run/gunicorn.sock api.wsgi:application &
+gunicorn --limit-request-line 8190 -t 0 -b unix:`pwd`/run/gunicorn.sock api.wsgi:application &
 nginx -c `pwd`/nginx/nginx.conf
