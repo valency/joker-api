f6ff5745388ce351441e191ca05c161f2c566cf6
Ye Ding <guiewy@gmail.com>
2015-07-25 19:57:36 +0800

    v0.7.25

--- joker/mathematics.py
+++ joker/mathematics.py
@@ -11,6 +11,7 @@ class Mathematics:
 
     @staticmethod
     def joker_kmeans(header, weight, pred_label, n_clusters, n_records, model):
+        CATEGORICAL_COLUMNS = ["id", "segment", "age", "gender", "is_member", "is_hrs_owner", "major_channel"]
         if model == 1:
             cust_obj = Customer1.objects
         elif model == 2:
@@ -22,46 +23,8 @@ class Mathematics:
         id_list = numpy.array([cust.id for cust in cust_set])
         for h in header:
             # Choose header
-            if h == "segment":
-                cust_column = numpy.array([cust.segment for cust in cust_set])
-                cust_column = categorical(cust_column, drop=True).argmax(1)
-            elif h == "age":
-                cust_column = numpy.array([cust.age for cust in cust_set])
-                cust_column = categorical(cust_column, drop=True).argmax(1)
-            elif h == "gender":
-                cust_column = numpy.array([cust.gender for cust in cust_set])
-                cust_column = categorical(cust_column, drop=True).argmax(1)
-            elif h == "yrs_w_club":
-                cust_column = numpy.array([cust.yrs_w_club for cust in cust_set])
-            elif h == "is_member":
-                cust_column = numpy.array([cust.is_member for cust in cust_set])
-                cust_column = categorical(cust_column, drop=True).argmax(1)
-            elif h == "is_hrs_owner":
-                cust_column = numpy.array([cust.is_hrs_owner for cust in cust_set])
-                cust_column = categorical(cust_column, drop=True).argmax(1)
-            elif h == "major_channel":
-                cust_column = numpy.array([cust.major_channel for cust in cust_set])
-                cust_column = categorical(cust_column, drop=True).argmax(1)
-            elif h == "mtg_num":
-                cust_column = numpy.array([cust.mtg_num for cust in cust_set])
-            elif h == "inv":
-                cust_column = numpy.array([cust.inv for cust in cust_set])
-            elif h == "div":
-                cust_column = numpy.array([cust.div for cust in cust_set])
-            elif h == "rr":
-                cust_column = numpy.array([cust.rr for cust in cust_set])
-            elif h == "end_bal":
-                cust_column = numpy.array([cust.end_bal for cust in cust_set])
-            elif h == "recharge_times":
-                cust_column = numpy.array([cust.recharge_times for cust in cust_set])
-            elif h == "recharge_amount":
-                cust_column = numpy.array([cust.recharge_amount for cust in cust_set])
-            elif h == "withdraw_times":
-                cust_column = numpy.array([cust.withdraw_times for cust in cust_set])
-            elif h == "withdraw_amount":
-                cust_column = numpy.array([cust.withdraw_amount for cust in cust_set])
-            else:
-                continue
+            cust_column = numpy.array([getattr(cust, h) for cust in cust_set])
+            if h in CATEGORICAL_COLUMNS: cust_column = categorical(cust_column, drop=True).argmax(1)
             # Stack to matrix
             if cust_matrix.size == 0:
                 cust_matrix = cust_column
@@ -73,8 +36,7 @@ class Mathematics:
         cust_matrix = numpy.nan_to_num(numpy.multiply(cust_matrix, numpy.array([numpy.array(weight)] * cust_set.count())))
         # Clustering
         kmeans_centres, kmeans_xtoc, kmeans_dist = kmeans(cust_matrix, randomsample(cust_matrix, n_clusters), metric="cosine")
-        # k_means = KMeans(init="k-means++", n_clusters=n_clusters)
-        # k_means.fit(cust_matrix)
+        # Output
         result = []
         for i in range(0, len(id_list)):
             entity = {
