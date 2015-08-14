ed75c4e1842a575e6d53d8073d5ffee1646f2f1a
Ye Ding <guiewy@gmail.com>
2015-08-14 21:14:15 +0800

    v0.8.14

--- joker-auth/serializers.py
+++ joker-auth/serializers.py
@@ -3,13 +3,6 @@ from rest_framework import serializers
 from models import *
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-
-
 class AccountSerializer(serializers.ModelSerializer):
-    user = UserSerializer
-
     class Meta:
         model = Account
