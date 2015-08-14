520e8aa933effec23bef1670e6ecb0ba2b412b71
Ye Ding <guiewy@gmail.com>
2015-08-14 20:08:40 +0800

    v0.8.14

--- joker-auth/views.py
+++ joker-auth/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import authenticate
-
+from django.views.decorators.csrf import csrf_exempt
 from serializers import *
 
 
