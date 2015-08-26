1d57b0e0d663930d4d21904205ca8bdf7afe5d33
Ye Ding <guiewy@gmail.com>
2015-08-26 19:55:38 +0800

    v0.8.26

--- joker_model_1/views.py
+++ joker_model_1/views.py
@@ -111,8 +111,10 @@ def add_cust_from_csv(request):
                     cust.decline_reason_code_1 = row["DECLINE_REASON_CODE_1"]
                     cust.decline_reason_code_2 = row["DECLINE_REASON_CODE_2"]
                     cust.decline_reason_code_3 = row["DECLINE_REASON_CODE_3"]
+                    inv_part = []
                     for i in range(0, CUST_INV_PART_COUNT, 1):
-                        cust.inv_part.append(float(row["INV" + str(i + 1)]))
+                        inv_part.append(float(row["INV" + str(i + 1)]))
+                    cust.inv_part = inv_part
                     cust.save()
                     count["success"] += 1
                 except TypeError:
