5dc26b72e5a7ff8ce769a48e6afbbf577c3a4e59
Ye Ding <guiewy@gmail.com>
2015-10-07 20:51:48 +0800

    v0.10.7

--- start-https.sh
+++ start-https.sh
@@ -2,4 +2,4 @@
 sudo nginx -s stop
 killall gunicorn
 gunicorn --limit-request-line 8190 -t 1800 -b unix:`pwd`/run/gunicorn.sock joker.wsgi:application &
-nginx -c `pwd`/nginx/nginx.conf
+sudo nginx -c `pwd`/nginx/nginx.conf
