3879ffd303ff79c5601d33aca918b4e3f0cd09ec
Ye Ding <guiewy@gmail.com>
2015-10-17 11:29:09 +0000

    v0.10.17

--- joker/settings.py
+++ joker/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'smartcube',
-        'USER': 'smartcube',
+        'USER': 'admin',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
