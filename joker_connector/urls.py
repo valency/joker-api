97222200863d6b72687c93418b5a3b333a0e1b5e
Ye Ding <guiewy@gmail.com>
2015-12-07 21:12:49 +0800

    v0.12.7

--- joker_connector/urls.py
+++ joker_connector/urls.py
@@ -5,5 +5,15 @@ import views
 urlpatterns = [
     url(r'list-modules/$', views.list_modules),
     url(r'list-profiles/$', views.list_profiles),
-    url(r'job-profile/$', views.job_profile)
+    url(r'job-profile/$', views.job_profile),
+    url(r'job-profile-push/$', views.job_profile_push),
+    url(r'job-profile-remove/$', views.job_profile_remove),
+    url(r'job-status/$', views.job_status),
+    url(r'job-kill/$', views.job_kill),
+    url(r'job-clear/$', views.job_clear),
+    url(r'job-set-num-slots/$', views.job_set_num_slots),
+    url(r'job-submit/$', views.job_submit),
+    url(r'job-remove/$', views.job_remove),
+    url(r'job-top/$', views.job_top),
+    url(r'job-reset/$', views.job_reset)
 ]
--- joker_connector/views.py
+++ joker_connector/views.py
@@ -21,16 +21,14 @@ SC_ADMIN = SmartCubeAdminClient(
 
 @api_view(['GET'])
 def list_modules(request):
-    return Response(SC_ADMIN.job_list_modules())
+    return Response(SC_ADMIN.job_modules())
 
 
 @api_view(['GET'])
 def list_profiles(request):
     if "module" in request.GET:
-        return Response({
-            "module": request.GET["module"],
-            "profiles": SC_ADMIN.job_list_profiles(request.GET["module"])
-        })
+        module = request.GET["module"]
+        return Response(SC_ADMIN.job_profiles(module))
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -43,7 +41,118 @@ def job_profile(request):
         return Response({
             "module": module,
             "profile": profile,
-            "content": SC_ADMIN.job_profile(module, profile)
+            "content": SC_ADMIN.job_profile_pull(module, profile)
         })
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def job_status(request):
+    if "id" in request.GET:
+        return Response(SC_ADMIN.job_status(request.GET["id"]))
+    else:
+        return Response(SC_ADMIN.job_status())
+
+
+@api_view(['GET'])
+def job_kill(request):
+    if "id" in request.GET:
+        job_id = request.GET["id"]
+        try:
+            SC_ADMIN.job_kill(job_id)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as exp:
+            return Response(str(exp), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def job_clear(request):
+    return Response(SC_ADMIN.job_clear())
+
+
+@api_view(['GET'])
+def job_set_num_slots(request):
+    if "slots" in request.GET:
+        slots = request.GET["slots"]
+        return Response({
+            "slots": SC_ADMIN.job_set_num_slots(slots)
+        })
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def job_profile_push(request):
+    if "module" in request.POST and "profile" in request.POST and "content" in request.POST:
+        module = request.POST["module"]
+        profile = request.POST["profile"]
+        content = request.POST["content"]
+        if "overwrite" in request.POST:
+            overwrite = request.POST["overwrite"] == "true"
+        else:
+            overwrite = False
+        return Response({
+            "id": SC_ADMIN.job_profile_push(module, profile, content, overwrite)
+        })
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def job_top(request):
+    if "id" in request.GET:
+        job_id = request.GET["id"]
+        try:
+            SC_ADMIN.job_top(job_id)
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except Exception as exp:
+            return Response(str(exp), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def job_remove(request):
+    if "id" in request.GET:
+        job_id = request.GET["id"]
+        try:
+            SC_ADMIN.job_remove(job_id)
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except Exception as exp:
+            return Response(str(exp), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def job_submit(request):
+    if "module" in request.GET and "profile" in request.GET:
+        module = request.GET["module"]
+        profile = request.GET["profile"]
+        return Response({
+            "id": SC_ADMIN.job_submit(module, profile)
+        })
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def job_reset(request):
+    return Response(SC_ADMIN.job_reset())
+
+
+@api_view(['GET'])
+def job_profile_remove(request):
+    if "module" in request.GET and "profile" in request.GET:
+        module = request.GET["module"]
+        profile = request.GET["profile"]
+        try:
+            SC_ADMIN.job_profile_remove(module, profile)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as exp:
+            return Response(str(exp), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
